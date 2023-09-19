5af64f3a62ea698d77c7d01acf7abe8734c465cc
тимур <timur@MacBook-Air-timur.local>
2023-09-19 21:29:14 +0300

    add solution to Task01

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Your code here!
+class Program
+{
+    static void Main()
+    {
+        string surname = Console.ReadLine();
+        string name = Console.ReadLine();
+        string fathersName = Console.ReadLine();
+        Console.Write("Фамилия: ", surname);
+        Console.Write("Имя: ", name);
+        Console.Write("Отчество: "), fathersName;
+    }
+}
