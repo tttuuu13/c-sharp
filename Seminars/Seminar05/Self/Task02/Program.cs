c0f6a5d7f460db5e1a7ee16ff32397ce6d68c7b5
тимур <timur@MacBook-Air-timur.local>
2023-09-20 20:13:51 +0300

    poop

---
+++ Seminars/Seminar05/Self/Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int i=0; int d=0; int o=0;
+        while (!(input=="0"||input=="-0"))
+        {
+            if (int.TryParse(input, out int r1))
+            {
+                i++;
+            }
+            else if (double.TryParse(input, out double r2))
+            {
+                d++;
+            }
+            else
+            {
+                o++;
+            }
+            input = Console.ReadLine();
+        }
+        Console.WriteLine(i);
+        Console.WriteLine(d);
+        Console.WriteLine(o);
+    }
+}
---
+++ Seminars/Seminar05/Self/Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        if (!(double.TryParse(Console.ReadLine(), out double a)))
+        {
+            Console.WriteLine("Wrong input");
+            return;
+        }
+        if (!(int.TryParse(Console.ReadLine(), out int n)))
+        {
+            Console.WriteLine("Wrong input");
+            return;
+        }
+        
+    }
+}
